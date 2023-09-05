7287e422c21f20b803d952ee3e850a54d479fff4
devmashum <dev.mashum@gmail.com>
2023-09-05 18:17:51 +0200

    new commit

---
+++ function.js
@@ -0,0 +1,14 @@
+function sleep(name, time) {
+    console.log(name, "is sleeping from" + time);
+}
+sleep("Faisal", " 10 PM");
+
+function myFunction(a, b) {
+    const multi = a * b;
+    return multi;
+}
+
+let fake = myFunction(4, 3);
+console.log(fake);
+
+console.log(fake * 5);
