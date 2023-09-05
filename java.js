e6515cb4d9bd9c482f23e74e115d53834d1991c4
devmashum <dev.mashum@gmail.com>
2023-09-05 17:38:52 +0200

    new commit

---
+++ java.js
@@ -0,0 +1,13 @@
+let text1 = "Faisal";
+let text2 = "Ahmed"
+
+console.log(text1 + " " + text2)
+
+let x = '2';
+let y = 30;
+console.log(x + y);
+
+let a = 20;
+let sa = "faisal";
+
+console.log(typeof a, typeof sa);
