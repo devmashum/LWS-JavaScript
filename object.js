0666e4cf1a4d64cd4c380920f165c1e1d03bb835
devmashum <dev.mashum@gmail.com>
2023-09-06 11:47:48 +0200

    new commit slice

--- object.js
+++ object.js
@@ -1,4 +1,9 @@
-const car = { type: "Fiat", model: "500", color: "white" };
+// const car = { type: "Fiat", model: "500", color: "white" };
 
-console.log(car.model);
-console.log(car["model"]);
+// console.log(car.model);
+// console.log(car["model"]);
+
+let x = ("Faisal");
+let y = new String("Faisal");
+
+console.log(x == y);
---
+++ string-methods.js
@@ -0,0 +1,10 @@
+// const a = {
+//     name: "Bangladesh";
+//     age: 47,
+//     are: 147570,
+// }
+
+// console.log(a.age);
+
+let str = "apple, banana, kiwi";
+console.log(str.slice(7, 13));
