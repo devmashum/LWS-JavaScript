bfc6a27429c05f165f44a26d8fe3a14d694ea09d
devmashum <dev.mashum@gmail.com>
2023-09-07 21:12:21 +0200

    new commit

--- number.js
+++ number.js
@@ -1,4 +1,7 @@
-let x = 120,
-    let y = 210,
-    let result = x * y;
-console.log(result); 
+// let x = 120,
+//     let y = 210,
+//     let result = x * y;
+// console.log(result);
+
+let a = 10e+3;
+console.log(a.toPrecision(8));
